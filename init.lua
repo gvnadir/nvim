1f329b649c7e209069be2b16761c415c6c6ee1ed
gvillalb <gianluca.nadir.villalba@capgemini.com>
2022-10-04 01:56:31 +0200

    add dashboard and transparency

--- init.lua
+++ init.lua
@@ -14,11 +14,12 @@ require "user.bufferline"
 require "user.lualine"
 require "user.toggleterm"
 require "user.colorizer"
-require "user.neosolarized"
+require "user.nvim-transparent"
+-- require "user.neosolarized"
 -- require "user.indent-blankline"
 
--- vim.o.background = "dark" 
--- vim.cmd([[colorscheme gruvbox]])
+ vim.o.background = "dark" 
+ vim.cmd([[colorscheme gruvbox]])
 
 local has = function(x)
   return vim.fn.has(x) == 1
--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -104,6 +104,14 @@ return packer.startup(function(use)
   use 'norcalli/nvim-colorizer.lua' -- css colorized hash
   use 'kyazdani42/nvim-web-devicons' -- add file icons
   -- use "lukas-reineke/indent-blankline.nvim" -- indent guides
+  use {
+    "startup-nvim/startup.nvim",
+    requires = {"nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim"},
+    config = function()
+      require"startup".setup()
+    end
+  }
+  use 'xiyaowong/nvim-transparent'
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
