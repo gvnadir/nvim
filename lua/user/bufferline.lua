01b32febdf99e015b49101837b8a0e572b12a2ff
gvillalb <gianluca.nadir.villalba@capgemini.com>
2022-10-04 20:10:44 +0200

    fix icon color

--- lua/user/bufferline.lua
+++ lua/user/bufferline.lua
@@ -57,7 +57,7 @@ bufferline.setup {
     -- end,
     offsets = { { filetype = "NvimTree", text = "", padding = 1 } },
     show_buffer_icons = true,
-    show_buffer_close_icons = true,
+    show_buffer_close_icons = false,
     show_close_icon = true,
     show_tab_indicators = true,
     persist_buffer_sort = true, -- whether or not custom sorted buffers should persist
@@ -77,15 +77,13 @@ bufferline.setup {
       bg = { attribute = "bg", highlight = "Normal" },
     },
     background = {
-      fg = { attribute = "fg", highlight = "TabLine" },
+      fg = { attribute = "fg", highlight = "" },
       bg = { attribute = "bg", highlight = "TabLine" },
     },
-
-    -- buffer_selected = {
-    --   guifg = {attribute='fg',highlight='#ff0000'},
-    --   guibg = {attribute='bg',highlight='#0000ff'},
-    --   gui = 'none'
-    --   },
+    buffer_selected = {
+      fg = { attribute = 'fg', highlight = '#ff0000' },
+      bg = { attribute = 'bg', highlight = '#0000ff' },
+    },
     buffer_visible = {
       fg = { attribute = "fg", highlight = "TabLine" },
       bg = { attribute = "bg", highlight = "TabLine" },
