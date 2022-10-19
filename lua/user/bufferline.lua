b45e84f597f9e7ab78b7c0bb90eea2e17a14f3a9
gvillalb <gianluca.nadir.villalba@capgemini.com>
2022-10-19 21:59:56 +0200

    customize lualine based on theme

--- lua/user/bufferline.lua
+++ lua/user/bufferline.lua
@@ -14,10 +14,10 @@ bufferline.setup {
     -- and so changing this is NOT recommended, this is intended
     -- as an escape hatch for people who cannot bear it for whatever reason
     indicator = "▎",
-    buffer_close_icon = "",
+    -- buffer_close_icon = "",
     -- buffer_close_icon = '',
     modified_icon = "●",
-    close_icon = "",
+    -- close_icon = "",
     -- close_icon = '',
     left_trunc_marker = "",
     right_trunc_marker = "",
@@ -58,7 +58,7 @@ bufferline.setup {
     offsets = { { filetype = "NvimTree", text = "", padding = 1 } },
     show_buffer_icons = true,
     show_buffer_close_icons = false,
-    show_close_icon = true,
+    show_close_icon = false,
     show_tab_indicators = true,
     persist_buffer_sort = true, -- whether or not custom sorted buffers should persist
     -- can also be a table containing 2 custom separators
--- lua/user/lualine.lua
+++ lua/user/lualine.lua
@@ -1,46 +1,56 @@
 local status, lualine = pcall(require, "lualine")
 if (not status) then return end
 
+local custom_theme = require 'lualine.themes.16color'
+
 lualine.setup {
   options = {
-    icons_enabled = false,
-    theme = 'auto',
-    -- theme = 'ayu_dark',
-    -- theme = 'molokai',
-    -- theme = 'papercolor_dark',
-    section_separators = { left = '', right = '' },
+    icons_enabled = true,
+    theme = custom_theme,
     component_separators = { left = '', right = '' },
-    disabled_filetypes = {}
+    section_separators = { left = '', right = '' },
+    disabled_filetypes = {
+      statusline = {},
+      winbar = {},
+    },
+    ignore_focus = {},
+    always_divide_middle = true,
+    globalstatus = false,
+    refresh = {
+      statusline = 1000,
+      tabline = 1000,
+      winbar = 1000,
+    }
   },
   sections = {
-    lualine_a = { 'mode' },
-    lualine_b = { 'branch' },
-    lualine_c = { {
-      'filename',
-      file_status = true, -- displays file status (readonly status, modified status)
-      path = 0 -- 0 = just filename, 1 = relative path, 2 = absolute path
+    lualine_a = { {
+      'mode',
+      color = { fg = 'white', bg = 'black' }
     } },
-    lualine_x = {
-      { 'diagnostics', sources = { "nvim_diagnostic" }, symbols = { error = ' ', warn = ' ', info = ' ',
-        hint = ' ' } },
-      'encoding',
-      'filetype'
-    },
-    lualine_y = { 'progress' },
-    lualine_z = { 'location' }
+    lualine_b = { { 'branch', '', 'diagnostics',
+      color = { fg = 'white', bg = 'black' }
+    } },
+    lualine_c = { 'filename' },
+    lualine_x = { 'encoding', 'fileformat', 'filetype' },
+    lualine_y = { { 'progress',
+
+      color = { fg = 'white', bg = 'black' }
+    } },
+    lualine_z = { { 'location',
+
+      color = { fg = 'white', bg = 'black' }
+    } }
   },
   inactive_sections = {
     lualine_a = {},
     lualine_b = {},
-    lualine_c = { {
-      'filename',
-      file_status = true, -- displays file status (readonly status, modified status)
-      path = 1 -- 0 = just filename, 1 = relative path, 2 = absolute path
-    } },
+    lualine_c = { 'filename' },
     lualine_x = { 'location' },
     lualine_y = {},
     lualine_z = {}
   },
   tabline = {},
-  extensions = { 'fugitive' }
+  winbar = {},
+  inactive_winbar = {},
+  extensions = {}
 }
