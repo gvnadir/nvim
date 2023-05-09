6b6df2a1937a4ca7a247f3f8b428ef643bc3cb7b
gvnadir <gianlucanadir@gmail.com>
2023-05-09 22:13:12 +0200

    move harpoon

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -79,18 +79,6 @@ vim.keymap.set("n", "<leader>dc", ":DiffviewClose<cr>")
 vim.keymap.set("n", "<leader>ss", ":vsplit<cr>")
 vim.keymap.set("n", "<leader>sh", ":split<cr>")
 
--- harpoon
-local mark = require("harpoon.mark")
-local ui = require("harpoon.ui")
-
-vim.keymap.set("n", "<leader>a", mark.add_file)
-vim.keymap.set("n", "<leader>h", ui.toggle_quick_menu)
-
-vim.keymap.set("n", "<leader>1", function() ui.nav_file(1) end)
-vim.keymap.set("n", "<leader>2", function() ui.nav_file(2) end)
-vim.keymap.set("n", "<leader>3", function() ui.nav_file(3) end)
-vim.keymap.set("n", "<leader>4", function() ui.nav_file(4) end)
-
 -- toggle ZenMode
 vim.keymap.set("n", "<leader>z", ":ZenMode<cr>")
 
