27d1e4d0a00b99e2d4d159993d27235043d29b2f
gvnadir <gianlucanadir@gmail.com>
2024-05-11 01:32:20 +0200

    update theme

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -5,56 +5,94 @@ return {
     name = 'plenary',
   },
   {
-    'navarasu/onedark.nvim',
-    opts = {
-      -- Main options --
-      style = 'dark', -- Default theme style. Choose between 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer' and 'light'
-      transparent = false, -- Show/hide background
-      term_colors = true, -- Change terminal color as per the selected theme style
-      ending_tildes = false, -- Show the end-of-buffer tildes. By default they are hidden
-      cmp_itemkind_reverse = false, -- reverse item kind highlights in cmp menu
-
-      -- toggle theme style ---
-      toggle_style_key = nil, -- keybind to toggle theme style. Leave it nil to disable it, or set it to a string, for example "<leader>ts"
-      toggle_style_list = { 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer', 'light' }, -- List of styles to toggle between
-
-      -- Change code style ---
-      -- Options are italic, bold, underline, none
-      -- You can configure multiple style with comma separated, For e.g., keywords = 'italic,bold'
-      code_style = {
-        comments = 'italic',
-        keywords = 'none',
-        functions = 'none',
-        strings = 'none',
-        variables = 'none',
-      },
-
-      -- Lualine options --
-      lualine = {
-        transparent = false, -- lualine center bar transparency
-      },
-
-      -- Custom Highlights --
-      colors = {}, -- Override default colors
-      highlights = {}, -- Override highlight groups
-
-      -- Plugins Config --
-      diagnostics = {
-        darker = true, -- darker colors for diagnostic
-        undercurl = true, -- use undercurl instead of underline for diagnostics
-        background = true, -- use background color for virtual text
-      },
-    },
+    'pappasam/papercolor-theme-slim',
     config = function(_, opts)
-      require('onedark').setup(opts)
-
       -- Load the colorscheme here
-      vim.cmd.colorscheme 'onedark'
-
-      -- You can configure highlights by doing something like
-      vim.cmd.hi 'Comment gui=none'
+      vim.cmd.colorscheme 'PaperColorSlim'
     end,
   },
+  -- {
+  --   'Mofiqul/vscode.nvim',
+  --   opts = {
+  --     -- Alternatively set style in setup
+  --     -- style = 'light'
+  --
+  --     -- Enable transparent background
+  --     transparent = true,
+  --
+  --     -- Enable italic comment
+  --     italic_comments = true,
+  --
+  --     -- Underline `@markup.link.*` variants
+  --     underline_links = true,
+  --
+  --     -- Disable nvim-tree background color
+  --     disable_nvimtree_bg = true,
+  --
+  --     -- Override colors (see ./lua/vscode/colors.lua)
+  --     color_overrides = {
+  --       vscLineNumber = '#FFFFFF',
+  --     },
+  --   },
+  --   config = function(_, opts)
+  --     -- Load the colorscheme here
+  --     vim.cmd.colorscheme 'vscode'
+  --
+  --     -- You can configure highlights by doing something like
+  --     vim.cmd.hi 'Comment gui=none'
+  --   end,
+  -- },
+  -- {
+  --   'navarasu/onedark.nvim',
+  --   opts = {
+  --     -- Main options --
+  --     style = 'dark', -- Default theme style. Choose between 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer' and 'light'
+  --     transparent = false, -- Show/hide background
+  --     term_colors = true, -- Change terminal color as per the selected theme style
+  --     ending_tildes = false, -- Show the end-of-buffer tildes. By default they are hidden
+  --     cmp_itemkind_reverse = false, -- reverse item kind highlights in cmp menu
+  --
+  --     -- toggle theme style ---
+  --     toggle_style_key = nil, -- keybind to toggle theme style. Leave it nil to disable it, or set it to a string, for example "<leader>ts"
+  --     toggle_style_list = { 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer', 'light' }, -- List of styles to toggle between
+  --
+  --     -- Change code style ---
+  --     -- Options are italic, bold, underline, none
+  --     -- You can configure multiple style with comma separated, For e.g., keywords = 'italic,bold'
+  --     code_style = {
+  --       comments = 'italic',
+  --       keywords = 'none',
+  --       functions = 'none',
+  --       strings = 'none',
+  --       variables = 'none',
+  --     },
+  --
+  --     -- Lualine options --
+  --     lualine = {
+  --       transparent = false, -- lualine center bar transparency
+  --     },
+  --
+  --     -- Custom Highlights --
+  --     colors = {}, -- Override default colors
+  --     highlights = {}, -- Override highlight groups
+  --
+  --     -- Plugins Config --
+  --     diagnostics = {
+  --       darker = true, -- darker colors for diagnostic
+  --       undercurl = true, -- use undercurl instead of underline for diagnostics
+  --       background = true, -- use background color for virtual text
+  --     },
+  --   },
+  --   config = function(_, opts)
+  --     require('onedark').setup(opts)
+  --
+  --     -- Load the colorscheme here
+  --     vim.cmd.colorscheme 'onedark'
+  --
+  --     -- You can configure highlights by doing something like
+  --     vim.cmd.hi 'Comment gui=none'
+  --   end,
+  -- },
   -- {
   --   'rose-pine/neovim',
   --   name = 'rose-pine',
