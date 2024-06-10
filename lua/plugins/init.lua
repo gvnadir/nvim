fbdd1ceba3af2ba7f3c806bb4a15d4095bf3b131
gvnadir <gianlucanadir@gmail.com>
2024-06-11 01:59:39 +0200

    feat: add kanagawa theme

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -94,83 +94,119 @@ return {
   --   end,
   -- },
   {
-    'rose-pine/neovim',
-    name = 'rose-pine',
+    'rebelot/kanagawa.nvim',
     opts = {
-      variant = 'auto', -- auto, main, moon, or dawn
-      dark_variant = 'main', -- main, moon, or dawn
-      dim_inactive_windows = false,
-      extend_background_behind_borders = true,
-
-      enable = {
-        terminal = true,
-        legacy_highlights = true, -- Improve compatibility for previous versions of Neovim
-        migrations = true, -- Handle deprecated options automatically
+      compile = false, -- enable compiling the colorscheme
+      undercurl = true, -- enable undercurls
+      commentStyle = { italic = false },
+      functionStyle = {},
+      keywordStyle = { italic = false },
+      statementStyle = { bold = true },
+      typeStyle = {},
+      transparent = false, -- do not set background color
+      dimInactive = false, -- dim inactive window `:h hl-NormalNC`
+      terminalColors = true, -- define vim.g.terminal_color_{0,17}
+      colors = { -- add/modify theme and palette colors
+        palette = {},
+        theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
       },
-
-      styles = {
-        bold = true,
-        italic = false,
-        transparency = false,
-      },
-
-      groups = {
-        border = 'muted',
-        link = 'iris',
-        panel = 'surface',
-
-        error = 'love',
-        hint = 'iris',
-        info = 'foam',
-        note = 'pine',
-        todo = 'rose',
-        warn = 'gold',
-
-        git_add = 'foam',
-        git_change = 'rose',
-        git_delete = 'love',
-        git_dirty = 'rose',
-        git_ignore = 'muted',
-        git_merge = 'iris',
-        git_rename = 'pine',
-        git_stage = 'iris',
-        git_text = 'rose',
-        git_untracked = 'subtle',
-
-        h1 = 'iris',
-        h2 = 'foam',
-        h3 = 'rose',
-        h4 = 'gold',
-        h5 = 'pine',
-        h6 = 'foam',
-      },
-
-      highlight_groups = {
-        -- Comment = { fg = "foam" },
-        -- VertSplit = { fg = "muted", bg = "muted" },
-      },
-
-      before_highlight = function(group, highlight, palette)
-        -- Disable all undercurls
-        -- if highlight.undercurl then
-        --     highlight.undercurl = false
-        -- end
-        --
-        -- Change palette colour
-        -- if highlight.fg == palette.pine then
-        --     highlight.fg = palette.foam
-        -- end
+      overrides = function(colors) -- add/modify highlights
+        return {}
       end,
+      theme = 'wave', -- Load "wave" theme when 'background' option is not set
+      background = { -- map the value of 'background' option to a theme
+        dark = 'wave', -- try "dragon" !
+        light = 'lotus',
+      },
     },
     config = function(_, opts)
-      require('rose-pine').setup(opts)
+      require('kanagawa').setup(opts)
 
       -- Load the colorscheme here
-      vim.cmd.colorscheme 'rose-pine'
+      vim.cmd.colorscheme 'kanagawa'
       -- You can configure highlights by doing something like
       vim.cmd.hi 'Comment gui=none'
     end,
   },
+
+  -- {
+  --   'rose-pine/neovim',
+  --   name = 'rose-pine',
+  --   opts = {
+  --     variant = 'auto', -- auto, main, moon, or dawn
+  --     dark_variant = 'main', -- main, moon, or dawn
+  --     dim_inactive_windows = false,
+  --     extend_background_behind_borders = true,
+  --
+  --     enable = {
+  --       terminal = true,
+  --       legacy_highlights = true, -- Improve compatibility for previous versions of Neovim
+  --       migrations = true, -- Handle deprecated options automatically
+  --     },
+  --
+  --     styles = {
+  --       bold = true,
+  --       italic = false,
+  --       transparency = false,
+  --     },
+  --
+  --     groups = {
+  --       border = 'muted',
+  --       link = 'iris',
+  --       panel = 'surface',
+  --
+  --       error = 'love',
+  --       hint = 'iris',
+  --       info = 'foam',
+  --       note = 'pine',
+  --       todo = 'rose',
+  --       warn = 'gold',
+  --
+  --       git_add = 'foam',
+  --       git_change = 'rose',
+  --       git_delete = 'love',
+  --       git_dirty = 'rose',
+  --       git_ignore = 'muted',
+  --       git_merge = 'iris',
+  --       git_rename = 'pine',
+  --       git_stage = 'iris',
+  --       git_text = 'rose',
+  --       git_untracked = 'subtle',
+  --
+  --       h1 = 'iris',
+  --       h2 = 'foam',
+  --       h3 = 'rose',
+  --       h4 = 'gold',
+  --       h5 = 'pine',
+  --       h6 = 'foam',
+  --     },
+  --
+  --     highlight_groups = {
+  --       -- Comment = { fg = "foam" },
+  --       -- VertSplit = { fg = "muted", bg = "muted" },
+  --     },
+  --
+  --     before_highlight = function(group, highlight, palette)
+  --       -- Disable all undercurls
+  --       -- if highlight.undercurl then
+  --       --     highlight.undercurl = false
+  --       -- end
+  --       --
+  --       -- Change palette colour
+  --       -- if highlight.fg == palette.pine then
+  --       --     highlight.fg = palette.foam
+  --       -- end
+  --     end,
+  --   },
+  --   config = function(_, opts)
+  --     require('rose-pine').setup(opts)
+  --
+  --     -- Load the colorscheme here
+  --     vim.cmd.colorscheme 'rose-pine'
+  --     -- You can configure highlights by doing something like
+  --     vim.cmd.hi 'Comment gui=none'
+  --   end,
+  -- },
 
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
 
